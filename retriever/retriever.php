5ec033c6e948277c7ef2b1025f67172d6b1fbc3d
Alex Cordonnier <alexjcord@gmail.com>
2015-03-23 16:02:19 -0400

    Working on retriever

--- retriever/retriever.php
+++ retriever/retriever.php
@@ -1,7 +1,24 @@
 <?php
-$data = file_get_contents("http://courses.illinois.edu/cisapp/explorer/catalog/2015/spring/CS.xml");
-$parsed = new SimpleXMLElement($data);
-foreach ($parsed->courses->course as $c) {
-    echo $parsed["id"] . " " . $c["id"] . ": " . $c . "\n";
+
+$link = mysql_connect("engr-cpanel-mysql.engr.illinois.edu", "classmat_www", "ClassMaster");
+if (!$link) {
+    die("Could not connect to MySQL: " . mysql_error());
 }
+echo "Connected successfully";
+mysql_close($link);
+
+// $term = "fall";
+// $year = "2015";
+// $sem = substr($term, 0, 2) . substr($year, 2, 2);
+
+// $data = file_get_contents("http://courses.illinois.edu/cisapp/explorer/schedule/".$year."/".$term."/CS.xml?mode=cascade");
+// $parsed = new SimpleXMLElement($data);
+
+// foreach ($parsed->cascadingCourses->cascadingCourse as $c) {
+//     foreach($c->detailedSections->detailedSection as $s) {
+//         $crn = $s["id"];
+//         $availability = $s->enrollmentStatus;
+//         echo $crn.": ".$availability."\n";
+//     }
+// }
 ?>
