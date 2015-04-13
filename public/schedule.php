2774835f38df57cab2e33e56347cc3bddeb94ac2
Alex Cordonnier <alexjcord@gmail.com>
2015-04-12 20:34:27 -0500

    Moved files around, integrated GJ's files

---
+++ public/schedule.php
@@ -0,0 +1,23 @@
+<?php include "../templates/header.php"; ?>
+
+<div class = "container">
+    <div class = "jumbotron">
+        <div class="row">
+            <div class="col-md-6">
+        		<h2>Search for a course</h2>
+                <div id="custom-search-input">
+                    <div class="input-group col-md-12">
+                        <input type="text" class="form-control input-lg" placeholder="CS 225" />
+                        <span class="input-group-btn">
+                            <button class="btn btn-info btn-lg" type="button">
+                                <span class="glyphicon glyphicon-search"></span>
+                            </button>
+                        </span>
+                    </div>
+                </div>
+            </div>
+    	</div>
+    </div>
+</div>
+
+<?php include "../templates/footer.php"; ?>
