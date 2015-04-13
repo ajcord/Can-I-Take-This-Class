f3ee527bddff638119721461694598785e225965
Alex Cordonnier <alexjcord@gmail.com>
2015-04-12 20:34:47 -0500

    Don't know why this didn't get committed

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php include "../templates/header.php"; ?>
+
+<div class="container">
+    <div class="jumbotron text-center">
+        <h1>ClassMaster</h1>
+        <p>Find out whether you'll get into the classes you want.</p>
+        <a class="btn btn-primary" href="schedule.php">Schedule Now!</a>
+        <a class="btn btn-default" href="courses.php">View Courses</a>
+        <a class="btn btn-default" href="about.php">Learn More</a>
+    </div>
+</div>
+
+<?php include "../templates/footer.php"; ?>
