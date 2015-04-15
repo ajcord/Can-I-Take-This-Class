49d124c905828d6890998f7b8e2e2dd1ca808037
Alex Cordonnier <alexjcord@gmail.com>
2015-04-14 21:59:35 -0500

    Use the template

--- public/retriever.php
+++ public/retriever.php
@@ -1,11 +1,6 @@
 <?php
 
-//Connect to MySQL
-$link = mysql_connect("engr-cpanel-mysql.engr.illinois.edu", "classmat_www", "ClassMaster");
-if (!$link) {
-    die("Could not connect to MySQL: " . mysql_error());
-}
-mysql_select_db("classmat_411");
+include "../templates/connect_mysql.php";
 
 $term = mysql_real_escape_string($_GET["term"]);
 $year = mysql_real_escape_string($_GET["year"]);
