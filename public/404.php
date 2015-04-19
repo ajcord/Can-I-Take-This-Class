1b7fccac41d5e020bc954978efa00d3d68927a73
Alex Cordonnier <alexjcord@gmail.com>
2015-04-19 11:57:53 -0500

    More spacing

--- public/404.php
+++ public/404.php
@@ -3,6 +3,7 @@
 <div class = "container">
     <div class = "jumbotron">
         <h1>Page not found (404)</h1>
+        <br><br>
         <p>The page you were looking for does not exist. We decided to show you
            this nice error message in its place.</p>
     </div>
