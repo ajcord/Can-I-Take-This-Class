3974f40f2f07fad65abf38ff88573284eedb74e7
Alex Cordonnier <alexjcord@gmail.com>
2015-04-19 11:57:24 -0500

    Updated 404 page

--- public/404.php
+++ public/404.php
@@ -2,9 +2,9 @@
 
 <div class = "container">
     <div class = "jumbotron">
-        <h1>404</h1>
-        <br><br>
-        <p>Page not found</p>
+        <h1>Page not found (404)</h1>
+        <p>The page you were looking for does not exist. We decided to show you
+           this nice error message in its place.</p>
     </div>
 </div>
 
