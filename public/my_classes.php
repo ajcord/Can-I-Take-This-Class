f978a317893ecd923d149b0d39df1e948108f4c6
Alex Cordonnier <alexjcord@gmail.com>
2015-04-19 14:59:36 -0500

    Put in a row

--- public/my_classes.php
+++ public/my_classes.php
@@ -11,13 +11,17 @@ include "../templates/header.php";
         <h1>My classes</h1>
         <br><br>
         <form class="form-horizontal" action="modify_classes.php" method="POST">
-            <label for="course-field">Add a class:</label>
-            <input id="course-field" name="course" type="text" class="form-control input-lg" placeholder="CS 225" />
-            <span class="input-group-btn">
-                <button id="add-course-button" class="btn btn-info btn-lg" type="submit" name="add-course-btn">
-                    <span class="glyphicon glyphicon-plus"></span>
-                </button>
-            </span>
+            <div class="row">
+                <div class="col-md-6">
+                    <label for="course-field">Add a class:</label>
+                    <input id="course-field" name="course" type="text" class="form-control input-lg" placeholder="CS 225" />
+                    <span class="input-group-btn">
+                        <button id="add-course-button" class="btn btn-info btn-lg" type="submit" name="add-course-btn">
+                            <span class="glyphicon glyphicon-plus"></span>
+                        </button>
+                    </span>
+                </div>
+            </div>
 
             <table class="table table-striped">
                 <thead>
