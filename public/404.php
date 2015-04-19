49bc6842900c0a859f82cb123fb3a565101c753f
Alex Cordonnier <alexjcord@gmail.com>
2015-04-19 11:51:39 -0500

    Added 404 page

---
+++ public/404.php
@@ -0,0 +1,11 @@
+<?php include "../templates/header.php"; ?>
+
+<div class = "container">
+    <div class = "jumbotron">
+        <h1>404</h1>
+        <br><br>
+        <p>Page not found</p>
+    </div>
+</div>
+
+<?php include "../templates/footer.php"; ?>
