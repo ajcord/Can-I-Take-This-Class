6348b1055e39435d6ac4fa0007ebbdc210368744
Alex Cordonnier <alexjcord@gmail.com>
2015-04-19 15:15:23 -0500

    Removed extra quote

--- public/modify_classes.php
+++ public/modify_classes.php
@@ -15,7 +15,7 @@ if (isset($add_course)) {
     $subject_code = mysql_real_escape_string(strtoupper(substr($course, 0, strlen($course) - 3)));
     $course_num = mysql_real_escape_string(substr($course, strlen($course) - 3));
 
-    $sql = "insert into wants (userid, subjectcode, coursenumber, semester) values (".$id.", '".$subject_code."', ".$course_num.", '".$sem."'')";
+    $sql = "insert into wants (userid, subjectcode, coursenumber, semester) values (".$id.", '".$subject_code."', ".$course_num.", '".$sem."')";
 
     $retval = mysql_query($sql);
     if (!$retval) {
