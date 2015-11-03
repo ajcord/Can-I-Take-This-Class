660dad6f7818090de3bb1b7eba39d619adb70d96
Alex Cordonnier <alexjcord@gmail.com>
2015-11-02 22:15:22 -0600

    Optimized API

--- public/api.php
+++ public/api.php
@@ -13,25 +13,31 @@ foreach($course_list as $course){
 
     $subject_code = mysql_real_escape_string(strtoupper(substr($course, 0, strlen($course) - 3)));
     $course_num = mysql_real_escape_string(substr($course, strlen($course) - 3));
-    
-    // Get enrollment data
 
-    $sql = "select sectiontype as type, enrollmentstatus as status, count(enrollmentstatus) as count from ".
-                "(select * from ".
-                    "(select * from availability where semester='$sem' order by timestamp desc) ".
-                "as sorted group by crn, semester) as latest ".
-            "inner join (select crn, semester, sectiontype, name from sections ".
-                "where subjectcode='$subject_code' and coursenumber=$course_num and semester='$sem') as sections ".
-            "using(crn, semester) group by type, status";
+    //Get the list of sections in this class
+    $section_list_sql = "select crn, sectiontype as type from sections ".
+                            "where subjectcode='$subject_code' and ".
+                            "coursenumber=$course_num and semester='$sem'";
 
-    $retval = mysql_query($sql)
+    $section_list_retval = mysql_query($section_list_sql)
         or die("Could not get availability data: ".mysql_error());
 
     $this_class = array();
-    while ($row = mysql_fetch_assoc($retval)) {
-        $type = $row["type"];
-        $status = $row["status"];
-        $count = $row["count"];
+    while ($section_row = mysql_fetch_assoc($section_list_retval)) {
+
+        $crn = $section_row["crn"];
+        $type = $section_row["type"];
+
+        //Get the most recent data for this section
+        $enrollment_sql = "select enrollmentstatus as status from ".
+                                "(select * from availability where crn=$crn and semester='$sem' order by timestamp desc) ".
+                            "as sorted group by crn, semester limit 1";
+
+        $enrollment_retval = mysql_query($enrollment_sql)
+            or die("Could not get availability data: ".mysql_error());
+
+        $enrollment_row = mysql_fetch_assoc($enrollment_retval);
+        $status = $enrollment_row["status"];
         if (!isset($this_class, $type)) {
             $type_arr = array();
             $this_class[$type] = $type_arr;
@@ -59,7 +65,8 @@ foreach($course_list as $course){
                 $status_str = "Unknown";
                 break;
         }
-        $this_class[$type][$status_str] = intval($count);
+
+        $this_class[$type][$status_str] += 1;
     }
 
     $courses_data[$course] = $this_class;
