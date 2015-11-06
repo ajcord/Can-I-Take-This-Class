a6a242bc5742eb0a0629f6688e5d65f4c845c05d
Alex Cordonnier <alexjcord@gmail.com>
2015-11-05 23:10:43 -0600

    Another fix

--- retriever/export.php
+++ retriever/export.php
@@ -6,12 +6,12 @@ $sem = "fa15";
 
 $sql = "select subjectcode, coursenumber, crn, timestamp, enrollmentstatus from ".
             "(select * from availability where semester='$sem') as data ".
-            "inner join (select * from sections where semester='sem') as classes ".
+            "inner join (select * from sections where semester='$sem') as classes ".
             "using(crn)";
 
 $retval = mysql_query($sql) or die("Could not export: ".mysql_error());
 
-echo "Writing data to dump.csv...";
+echo "Writing data to dump.csv...\n";
 
 $file = fopen("dump.csv", 'w');
 
@@ -21,6 +21,6 @@ while ($row = mysql_fetch_row($retval)) {
 
 fclose($file);
 
-echo "Exported data to dump.csv";
+echo "Exported data to dump.csv\n";
 
 ?>
