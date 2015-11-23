e5c067780f73c5700d311d5718997af2b84ed76e
Alex Cordonnier <alexjcord@gmail.com>
2015-11-22 19:34:42 -0500

    Combine queries

--- templates/analyze.php
+++ templates/analyze.php
@@ -55,16 +55,10 @@ function get_offset_into_registration($date) {
  */
 function get_last_week($sem, $date) {
 
-    //Fetch an example CRN to make the availability query much faster
-    $crn_sql = "select crn from sections where semester='$sem' limit 1";
-
-    $crn_retval = mysql_query($crn_sql)
-        or die("Could not get a CRN: ".mysql_error());
-
-    $crn = mysql_fetch_assoc($crn_retval)["crn"];
-
+    //Fetches a sample CRN to make the availability query much faster
     $last_week_sql = "select floor(datediff(max(timestamp), '$date')/7) as week ".
-                        "from availability where semester='$sem' and crn='$crn'";
+                        "from availability where semester='$sem' and ".
+                        "crn=(select crn from sections where semester='$sem' limit 1)";
 
     $last_week_retval = mysql_query($last_week_sql)
         or die("Could not get last week: ".mysql_error());
