d6c084bb340c45db5db3897f7a76bd89547232eb
Alex Cordonnier <alexjcord@gmail.com>
2015-11-23 19:04:23 -0500

    Moved search field

--- public/index.php
+++ public/index.php
@@ -3,12 +3,6 @@
 <div class="container">
     <div class="jumbotron text-center">
         <h1>ClassMaster</h1>
-        <p>Find out whether you'll get into the classes you want.</p>
-        <a class="btn btn-primary" href="my_classes.php">My Classes</a>
-        <a class="btn btn-default" href="courses.php">View Courses</a>
-        <a class="btn btn-default" href="about.php">Learn More</a>
-
-        <br>
         
         <form class="form-horizontal" action="historical.php" method="GET">
             <div class="row">
@@ -25,6 +19,13 @@
                 </div>
             </div>
         </form>
+
+        <br><br>
+
+        <p>Find out whether you'll get into the classes you want.</p>
+        <a class="btn btn-primary" href="my_classes.php">My Classes</a>
+        <a class="btn btn-default" href="courses.php">View Courses</a>
+        <a class="btn btn-default" href="about.php">Learn More</a>
     </div>
 </div>
 
