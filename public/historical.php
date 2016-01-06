480c186962527eaa299d11ccd5ad3041c14c13d7
Alex Cordonnier <alexjcord@gmail.com>
2016-01-05 19:37:46 -0500

    Added search box template

--- public/historical.php
+++ public/historical.php
@@ -65,28 +65,8 @@ if (mysql_num_rows($semesters_retval) == 0) {
 
 <div class="container">
     <div class="jumbotron">
-        <form class="form-horizontal" action="historical.php" method="GET">
-            <div class="row">
-                <div class="col-md-6">
-                    <h2>Search for a class or subject</h2>
-                    <div class="input-group col-md-12">
-                        <input id="search-field" name="q" type="text" class="form-control input-lg" placeholder="e.g. CS 225, PHYS, etc."
-<?php if (!is_null($_GET["q"])): ?>
-                            value="<?php echo $_GET['q'] ?>"
-<?php endif ?>
-                        />
-<?php if (!is_null($_GET["semester"])): ?>
-                        <input type="hidden" name="semester" value="<?php echo $_GET['semester'] ?>" />
-<?php endif ?>
-                        <span class="input-group-btn">
-                            <button id="search-button" class="btn btn-info btn-lg" type="submit">
-                                <span class="glyphicon glyphicon-search"></span>
-                            </button>
-                        </span>
-                    </div>
-                </div>
-            </div>
-        </form>
+        <?php include "../templates/search.php" ?>
+
 <?php if (!is_null($_GET["q"])): ?>
         <br>
         <p>
