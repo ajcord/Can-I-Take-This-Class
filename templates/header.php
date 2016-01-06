20e72c4e4505a9e39df5fb0a25cf48d9a1dd7b85
Alex Cordonnier <alexjcord@gmail.com>
2016-01-05 19:58:31 -0500

    Simplified nav

--- templates/header.php
+++ templates/header.php
@@ -26,22 +26,8 @@
 <body>
     <nav class="navbar navbar-default navbar-fixed-top">
         <div class="container-fluid">
-            <!-- Combine brand with collapse button for small screens -->
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="index.php">Can I Take This Class?</a>
+                <a class="navbar-brand" href="index.php">Home</a>
             </div>
-
-            <!-- Collect nav items on small screens -->
-            <div class="collapse navbar-collapse" id="navbar-collapse">
-                <ul class="nav navbar-nav">
-                    <li><a href="historical.php">Historical data</a></li>
-                </ul>
-            </div><!-- /.navbar-collapse -->
         </div>
     </nav>
