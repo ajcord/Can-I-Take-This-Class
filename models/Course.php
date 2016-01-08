1487105af4d08d28053f91c920fcd1a801dc92c9
Alex Cordonnier <alexjcord@gmail.com>
2016-01-07 23:42:46 -0500

    Fixed input validation & conversion

--- models/Course.php
+++ models/Course.php
@@ -28,7 +28,7 @@ class Course {
             throw new UnexpectedValueException("Invalid subject code: $subject_code");
         }
         
-        if (is_int($course_num) && $course_num > 0 && $course_num < 999) {
+        if (is_int($course_num) && $course_num > 0 && $course_num <= 999) {
             $this->course_num = $course_num;
         } else {
             throw new UnexpectedValueException("Invalid course number: $course_num");
