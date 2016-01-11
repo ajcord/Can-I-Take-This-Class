f2e5a3faf140cb1089db9523658c806d43b4f830
Alex Cordonnier <alexjcord@gmail.com>
2016-01-11 14:55:02 -0500

    Remove deploy.sh

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ "$1" == "dev" ]; then
-    rsync -av -P --exclude=".*" . cordonn2_dev@dev.canitakethisclass.com:~/project --delete
-elif [ "$1" == "prod" ]; then
-    rsync -av -P --exclude=".*" . cordonn2@canitakethisclass.com:~/project --delete
-else
-    echo "Usage: deploy.sh dev|prod"
-fi
