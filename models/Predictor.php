a9aa5f4adfdc985c5c8d7cfab836e3d7f2eb0411
Alex Cordonnier <alexjcord@gmail.com>
2016-01-10 22:17:56 -0500

    Use previous semesters to predict future ones

--- models/Predictor.php
+++ models/Predictor.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once "Semester.php";
+
 /**
  * Makes statistical predictions of class availability.
  */
@@ -27,30 +29,19 @@ class Predictor {
      */
     public function getOverallLikelihood() {
         
-        // Format of return value. Values will be replaced.
-        $result = [
-            "on_date" => [
-                "percent" => 1,
-                "error" => 0,
-            ],
-            "after_date" => [
-                "percent" => 1,
-                "error" => 0,
-            ],
-        ];
+        $result = [];
 
-        // TODO: handle when course is not offered
+        // Get a list of equivalent registration dates
+        $adjusted_dates = Semester::adjustDate($this->dbh, $this->date);
 
-        // Get stats on the date
-        
+        // Get stats on the given date
         $sql = <<<SQL
             SELECT sectiontype,
                 percent,
-                n,
                 SQRT(percent*(1-percent)/n) AS error
             FROM (
                 SELECT sectiontype,
-                    -- Get the percentage of results with enrollmentstatus > 0
+                    -- Hack to get the percentage of non-closed sections
                     AVG(IF(enrollmentstatus>0,1,0)) AS percent,
                     COUNT(*) AS n
                 FROM availability
@@ -67,31 +58,17 @@ SQL;
         $stmt = $this->dbh->prepare($sql);
         $stmt->bindValue(":subject_code", $this->course->getSubjectCode());
         $stmt->bindValue(":course_num", $this->course->getCourseNumber());
-        $stmt->bindValue(":date", $this->date->format("Y-m-d"));
 
-        $stmt->execute();
-
-        foreach ($stmt as $row) {
-            
-            $percent = floatval($row["percent"]);
-            $error = floatval($row["error"]);
-
-            if ($percent < $result["on_date"]["percent"]) {
-                $result["on_date"]["percent"] = $percent;
-                $result["on_date"]["error"] = $error;
-            }
-        }
+        $result["on_date"] = $this->calculateLikelihood($stmt, $adjusted_dates);
         
-        // Get stats for after the date
-        
+        // Get stats after the given date
         $sql = <<<SQL
             SELECT sectiontype,
                 percent,
-                n,
                 SQRT(percent*(1-percent)/n) AS error
             FROM (
                 SELECT sectiontype,
-                    -- Get the percentage of results with enrollmentstatus > 0
+                    -- Hack to get the percentage of non-closed sections
                     AVG(IF(enrollmentstatus>0,1,0)) AS percent,
                     COUNT(*) AS n
                 FROM availability
@@ -113,19 +90,72 @@ SQL;
         $stmt = $this->dbh->prepare($sql);
         $stmt->bindValue(":subject_code", $this->course->getSubjectCode());
         $stmt->bindValue(":course_num", $this->course->getCourseNumber());
-        $stmt->bindValue(":date", $this->date->format("Y-m-d"));
 
-        $stmt->execute();
+        $result["after_date"] = $this->calculateLikelihood($stmt, $adjusted_dates);
 
-        foreach ($stmt as $row) {
-            
-            $percent = floatval($row["percent"]);
-            $error = floatval($row["error"]);
+        return $result;
+    }
 
-            if ($percent < $result["after_date"]["percent"]) {
-                $result["after_date"]["percent"] = $percent;
-                $result["after_date"]["error"] = $error;
+    /**
+     * Given a SQL statemtent handle to get a single semester's percentage
+     * for each section type, calculates the weighted average of
+     * several semester's percentages along with the error.
+     */
+    private function calculateLikelihood($stmt, $adjusted_dates) {
+
+        $result = [
+            "percent" => 0,
+            "error" => 0,
+        ];
+
+        // Must be bound here because of scope
+        $stmt->bindParam(":date", $dateFormatted);
+
+        $sem_count = 0;
+
+        foreach ($adjusted_dates as $date) {
+
+            $dateFormatted = $date->format("Y-m-d");
+            $stmt->execute();
+
+            // Pick the smallest percentage and its corresponding error
+            $min_percent = 1;
+            $min_percent_error = 0;
+            $offered = false;
+
+            foreach ($stmt as $row) {
+                
+                $percent = floatval($row["percent"]);
+                $error = floatval($row["error"]);
+
+                if ($percent < $min_percent) {
+                    $min_percent = $percent;
+                    $min_percent_error = $error;
+                }
+
+                $offered = true;
             }
+
+            if (!$offered) {
+                // Don't calculate for semesters it wasn't offered
+                continue;
+            }
+
+            // Remember:
+            // E(aX + bY) = a * E(X) + b * E(Y)
+            // Var(aX + bY) = a^2 * Var(X) + b^2 * Var(Y)
+            // SD(aX + bY) = sqrt(a^2 * SD(X)^2 + b^2 * SD(Y)^2)
+            $result["percent"] += $min_percent;
+            $result["error"] = $result["error"]**2 + $min_percent_error**2;
+
+            if ($sem_count > 0) {
+                $result["percent"] *= 0.5;
+                $result["error"] *= 0.25;
+            }
+
+            $result["error"] = sqrt($result["error"]);
+
+            $sem_count++;
         }
 
         return $result;
