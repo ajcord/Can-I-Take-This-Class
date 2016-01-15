68c70adb64b82f80717f6d5cbfc63667a295f710
Alex Cordonnier <alexjcord@gmail.com>
2016-01-15 15:50:51 -0500

    Added GA

--- templates/footer.php
+++ templates/footer.php
@@ -13,5 +13,19 @@
     </footer>
 
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      <? if (substr($_SERVER["HTTP_HOST"], 0, 3) == "dev"): ?>
+        ga('create', 'UA-72483548-2', 'auto');
+      <? else: ?>
+        ga('create', 'UA-72483548-1', 'auto');
+      <? endif ?>
+      ga('send', 'pageview');
+    </script>
 </body>
 </html>
