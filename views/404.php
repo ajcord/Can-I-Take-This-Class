39113e9004e499f304d89de92c2129b37935fb47
Alex Cordonnier <alexjcord@gmail.com>
2016-10-18 22:54:12 -0500

    Improved 404 page

--- views/404.php
+++ views/404.php
@@ -1,12 +1,30 @@
 <?php include "../templates/header.php"; ?>
 
-<div class = "container">
-    <div class = "jumbotron">
-        <h1>Page not found (404)</h1>
-        <br><br>
-        <p>The page you were looking for does not exist. We decided to show you
-           this nice error message in its place.</p>
+<div class="jumbotron text-center">
+    <h1>Page not found</h1>
+    <div class="container">
+        <p>
+            Sorry, but the page you were looking for doesn't exist.
+        </p>
     </div>
+</div>
+
+<div class="container">
+
+    <p>
+        This could be due to:
+    </p>
+
+    <ul>
+        <li>Clicking an outdated link</li>
+        <li>Mistyping the website address</li>
+        <li>A mistake on our end</li>
+    </ul>
+
+    <p>
+        You can try <a href="/">going home</a>.
+    </p>
+
 </div>
 
 <?php include "../templates/footer.php"; ?>
